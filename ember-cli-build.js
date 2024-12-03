13e2768a1eb034b7a0a61f1c82e8aad3d024b8a6
iamhariharanvj <hariharanvj2003@gmail.com>
2024-12-03 15:02:14 +0530

    enabled typescript sourcemaps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
+    babel: { sourceMaps: 'inline' },
 
     // Add options here
   });
